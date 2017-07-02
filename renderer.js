df69f71ec86eeaf383e3b8ca73210bb5f9fe5236
darylbutler <darylbutler@gmail.com>
2017-07-01 22:23:24 -0400

    initial commit

---
+++ renderer.js
@@ -0,0 +1,59 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+// Setup clicl events
+$(".tTag").click(function(e) {
+    console.log('click!: ' + e);
+    if ($("#myChart").hasClass("hide")) {
+        $("#myChart").removeClass("hide");
+        $("#temp").addClass("hide");
+
+        createChart();
+    } else {
+        $("#myChart").addClass("hide");
+        $("#temp").removeClass("hide");
+
+        $("#myChart").html = "";
+    }
+});
+
+function createChart() {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false
+                    }
+                }]
+            }
+        }
+    });
+}
